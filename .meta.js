c22405f13995ee552036b07fa44a72cfe3d664ef
Alvezxity <Naray-aa@student.oxley.vic.edu.au>
2025-06-26 20:54:40 +1000

    Create .meta.js
    New modified file from the KaHack! File.

---
+++ .meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         KaHackX!
+// @namespace    https://github.com/Alvezxity
+// @version      1.0.38
+// @description  KaHackX! For Kahoot to view correct answers
+// @author       Alvezxity; https://github.com/Alvezxity
+// @match        https://kahoot.it/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
+// @grant        none
+// @updateURL    https://github.com/Alvezxity/KaFraud/raw/main/KaHackX!.meta.js
+// @downloadURL  https://github.com/Alvezxity/KaFraud/raw/main/KaHackX!.user.js
+// ==/UserScript==
