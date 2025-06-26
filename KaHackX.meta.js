73ce668b99a7a01427635fcb9256b53359861991
Alvezxity <Naray-aa@student.oxley.vic.edu.au>
2025-06-26 21:05:13 +1000

    Create KaHackX.meta.js
    My Modified Version of the KaHack! Made by Jokeri4444

---
+++ KaHackX.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         KaHackX!
+// @namespace    https://github.com/Alvezxity
+// @version      1.0.38
+// @description  KaHackX! For Kahoot to view correct answers
+// @author       Alvezxity; https://github.com/Alvezxity
+// @match        https://kahoot.it/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
+// @grant        none
+// ==/UserScript==
